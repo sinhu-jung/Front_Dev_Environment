3ad88b143599b6982df832c637aad9bbdbad7140
sinhu <tlsgn26@naver.com>
2023-07-05 22:04:13 +0900

    eslint 설정 연습

---
+++ Lint-Practice/src/app.js
@@ -0,0 +1,9 @@
+import MainController from './controllers/MainController.js';
+
+import './app.scss';
+
+const foo = '';
+
+document.addEventListener("DOMContentLoaded", ()=>{
+new MainController(); 
+})
