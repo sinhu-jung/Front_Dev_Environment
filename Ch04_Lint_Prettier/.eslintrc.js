e777490f3116d2e8c7c2b9fa5a5e4587c848b74d
sinhu <tlsgn26@naver.com>
2023-07-07 09:35:27 +0900

    프리티어 연습

--- Ch04_Lint_Prettier/.eslintrc.js
+++ Ch04_Lint_Prettier/.eslintrc.js
@@ -1,28 +1,24 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": ["eslint:recommended",  "plugin:prettier/recommended"],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-      ],
-    "rules": {
-    },
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {},
+};
--- Ch04_Lint_Prettier/app.js
+++ Ch04_Lint_Prettier/app.js
@@ -1,3 +1,1 @@
-var foo = "";
-
 console.log();
